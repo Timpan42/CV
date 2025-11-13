8457dac03e6cb2db2933eb9a8f5a382230135290
Timpan42 <Timpan42@users.noreply.github.com>
2025-11-13 13:44:14 +0000

    deploy: ac8eb45262aec8a317dc3ba821e6da06a962aaa8

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,60 @@
-window.onload = function () {
-  const menu_btn = document.querySelector('.hamburger');
+const allIframeCards = [];
+var useImage;
 
+
+class IframCard {
+  constructor(object, image, iframe) {
+    this.object = object;
+    this.image = image;
+    this.iframe = iframe;
+  }
+}
+
+window.onload = function () {
+  var allTest = document.querySelectorAll("*[id = iframe-img]");
+  const menu_btn = document.querySelector('.hamburger');
   const mobile_nav = document.querySelector('.mobile-nav');
+
+  allTest.forEach(item => {
+    allIframeCards.push(new IframCard(item, item.querySelector("#img"), item.querySelector("#iframe")));
+  });
+
+  SetIframeContent();
 
   menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_nav.classList.toggle('is-active');
   })
+}
+
+addEventListener("resize", (event) => {
+  SetIframeContent();
+})
+
+function SetIframeContent()
+{
+  var tmp = window.innerWidth <= 1350;
+
+  if (useImage == tmp) {
+    return;
+  }
+  useImage = tmp;
+
+  if (useImage) {
+
+    allIframeCards.forEach(element => {
+      console.log(element);
+      element.object.innerHTML = "";
+      element.object.appendChild(element.image);
+    });
+  }
+  else {
+    allIframeCards.forEach(element => {
+      console.log(element.iframe);
+      element.object.innerHTML = "";
+      element.object.appendChild(element.iframe)
+    });
+  }  
 }
 
 function ToggleContent(itemId) {
